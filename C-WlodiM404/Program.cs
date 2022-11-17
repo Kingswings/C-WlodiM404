9b26c757b077ede71bc4b42c7b1f4df4f07ca0c1
Sidney Hyde <shyde@online.gibz.ch>
2022-11-17 08:10:51 +0100

    Initial commit

---
+++ C-WlodiM404/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_WlodiM404
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
